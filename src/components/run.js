3ed06cb3cb60ca47db0aadf572e2687f6a80a790
korneliusz <korneliusz.caputa@siili.fi>
2015-11-02 21:01:59 +0100

    Remove console.log.

--- src/components/run.js
+++ src/components/run.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Main';
 
-console.log('test');
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'));
